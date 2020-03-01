531cbaac22032d33a966a731cc43fb696ca96b3c
phylax <dfusi@hotmail.com>
2020-03-01 14:04:36 +0100

    Added XML thesaurus parser

---
+++ Mq.Migration/XmlThesaurusParser.cs
@@ -0,0 +1,92 @@
+﻿using Cadmus.Core.Config;
+using Fusi.Tools.Text;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace Mq.Migration
+{
+    public sealed class XmlThesaurusParser : IHasLogger
+    {
+        private readonly TextCutterOptions _options;
+        private readonly Regex _wsRegex;
+
+        public XmlThesaurusParser()
+        {
+            _options = new TextCutterOptions
+            {
+                LimitAsPercents = false,
+                LineFlattening = true,
+                MaxLength = 30,
+                MinusLimit = 5,
+                PlusLimit = 5
+            };
+            _wsRegex = new Regex(@"\s+");
+        }
+
+        /// <summary>
+        /// Gets or sets the logger.
+        /// </summary>
+        public ILogger Logger { get; set; }
+
+        private string ReduceLabel(string label)
+        {
+            label = _wsRegex.Replace(label, " ").Trim();
+            int i = label.LastIndexOf('[');
+            string head = TextCutter.Cut(label, _options);
+            return i > -1 ? head + (label.Substring(i)) : head;
+        }
+
+        private Thesaurus ParseSource(XElement sourceElem, string id,
+            bool authors)
+        {
+            Thesaurus thesaurus = new Thesaurus(
+                $"apparatus-{(authors? "authors":"witnesses")}.{id}@en");
+
+            foreach (XElement child in sourceElem
+                .Elements(XmlHelper.TEI + (authors? "bibl" : "witness")))
+            {
+                var entry = new ThesaurusEntry(
+                    child.Attribute(XmlHelper.XML + "id").Value,
+                    ReduceLabel(child.Value));
+                if (entry.Value.Length == 0)
+                {
+                    Logger?.LogWarning(
+                        $"Empty entry for ID {entry.Id} in document {id}");
+                }
+                thesaurus.AddEntry(entry);
+            }
+
+            return thesaurus;
+        }
+
+        public Thesaurus[] Parse(XDocument doc, string id)
+        {
+            if (doc == null) throw new ArgumentNullException(nameof(doc));
+            if (id == null) throw new ArgumentNullException(nameof(id));
+
+            id = id.ToLowerInvariant();
+
+            XElement sourceDescElem = doc.Root
+                .Element(XmlHelper.TEI + "teiHeader")
+                .Element(XmlHelper.TEI + "fileDesc")
+                .Elements(XmlHelper.TEI + "sourceDesc")
+                .FirstOrDefault(e => e.Elements()
+                    .Any(c => c.Name == XmlHelper.TEI + "listWit"));
+
+            Thesaurus witnesses = ParseSource(sourceDescElem
+                .Element(XmlHelper.TEI + "listWit")
+                .Element(XmlHelper.TEI + "listWit"),
+                id, false);
+
+            Thesaurus authors = ParseSource(sourceDescElem
+                .Element(XmlHelper.TEI + "listBibl")
+                .Element(XmlHelper.TEI + "listBibl"),
+                id, true);
+
+            return new[] { witnesses, authors };
+        }
+    }
+}
---
+++ Mqutil/Commands/ImportThesauriCommand.cs
@@ -0,0 +1,140 @@
+﻿using Cadmus.Core.Config;
+using Microsoft.Extensions.CommandLineUtils;
+using Mq.Migration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mqutil.Commands
+{
+    public sealed class ImportThesauriCommand : ICommand
+    {
+        private readonly string _inputFileMask;
+        private readonly string _outputFilePath;
+
+        public ImportThesauriCommand(string inputFileMask, string outputFilePath)
+        {
+            _inputFileMask = inputFileMask ??
+                throw new ArgumentNullException(nameof(inputFileMask));
+            _outputFilePath = outputFilePath ??
+                throw new ArgumentNullException(nameof(outputFilePath));
+        }
+
+        /// <summary>
+        /// Configures the specified command.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="options">The options.</param>
+        /// <exception cref="ArgumentNullException">command</exception>
+        public static void Configure(CommandLineApplication command,
+            AppOptions options)
+        {
+            if (command == null)
+                throw new ArgumentNullException(nameof(command));
+
+            command.Description = "Import thesauri from MQDQ app documents " +
+                "into the specified JSON file";
+            command.HelpOption("-?|-h|--help");
+
+            CommandArgument inputArgument = command.Argument("[input]",
+                "The input entries files mask");
+            CommandArgument outputArgument = command.Argument("[output]",
+                "The output JSON file path");
+
+            command.OnExecute(() =>
+            {
+                options.Command = new ImportThesauriCommand(
+                    inputArgument.Value,
+                    outputArgument.Value);
+                return 0;
+            });
+        }
+
+        public Task Run()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("IMPORT THESAURI\n");
+            Console.ResetColor();
+            Console.WriteLine(
+                $"Input:  {_inputFileMask}\n" +
+                $"Output: {_outputFilePath}\n");
+
+            XmlThesaurusParser parser = new XmlThesaurusParser();
+            // TODO: logger
+            int inputFileCount = 0;
+            int witCount = 0, authCount = 0;
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                IgnoreNullValues = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            using (StreamWriter writer = new StreamWriter(_outputFilePath,
+                false, Encoding.UTF8))
+            {
+                writer.WriteLine("[");
+
+                // for each input document
+                foreach (string filePath in Directory.GetFiles(
+                    Path.GetDirectoryName(_inputFileMask),
+                    Path.GetFileName(_inputFileMask))
+                    .OrderBy(s => s))
+                {
+                    // load document
+                    string inputFileName = Path.GetFileNameWithoutExtension(
+                        filePath);
+                    Console.WriteLine(filePath);
+                    inputFileCount++;
+                    XDocument doc = XDocument.Load(filePath,
+                        LoadOptions.PreserveWhitespace);
+
+                    // parse items
+                    string docId = Path.GetFileNameWithoutExtension(filePath)
+                        .Replace("-app", "");
+                    Thesaurus[] thesauri = parser.Parse(doc, docId);
+                    witCount += thesauri[0].GetEntries().Count;
+                    authCount += thesauri[1].GetEntries().Count;
+
+                    for (int i = 0; i < thesauri.Length; i++)
+                    {
+                        SerializableThesaurus th = new SerializableThesaurus
+                        {
+                            Id = thesauri[i].Id
+                        };
+                        foreach (ThesaurusEntry entry in thesauri[i].GetEntries())
+                            th.Entries.Add(entry);
+
+                        string json = JsonSerializer.Serialize(th, options);
+                        writer.WriteLine(json);
+                    }
+                }
+                writer.WriteLine("]");
+                writer.Flush();
+            }
+
+            Console.WriteLine($"\nInput documents: {inputFileCount}");
+            Console.WriteLine($"Witnesses: {witCount}");
+            Console.WriteLine($"Authors: {authCount}");
+
+            return Task.CompletedTask;
+        }
+    }
+
+    internal sealed class SerializableThesaurus
+    {
+        public string Id { get; set; }
+
+        public IList<ThesaurusEntry> Entries { get; }
+
+        public SerializableThesaurus()
+        {
+            Entries = new List<ThesaurusEntry>();
+        }
+    }
+}
